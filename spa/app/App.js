75170eed0549eccf440a775b2b15196bc0880290
emersonpalacio <emersonpalaciootalvaro@gmail.com>
2021-03-04 11:08:44 -0500

    pericionesAsy

--- spa/app/App.js
+++ spa/app/App.js
@@ -1,4 +1,44 @@
+import api from './helpers/wp_api.js'
+import {ajax} from './helpers/ajax.js'
+
 export function App(){
     document.getElementById("root").innerHTML=`<h1> hola desde emerson App</h1>`
+    
+    ajax({
+        url:api.POST,
+        cbSuccess : (posts)=>{
+            console.log(posts)
+        }
+    });
+
+    ajax({
+        url:api.CATEGORIES,
+        cbSuccess : (categories)=>{
+            console.log(categories)   
+        }
+    });
+    
+    ajax({
+        url:api.POSTS,
+        cbSuccess : (posts)=>{
+            console.log(posts)   
+        }
+    });
+
+    ajax({
+        url:api.SEARCH,
+        cbSuccess : (search)=>{
+            console.log(search)   
+        }
+    });
+
+    ajax({
+        url:api.SITE,
+        cbSuccess : (site)=>{
+            console.log(site)   
+        }
+    });
+
+    console.log(api)
     
 }
