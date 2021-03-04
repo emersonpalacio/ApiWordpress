ff999277ee0d15ca620c057bff617d55ebdaf795
emersonpalacio <emersonpalaciootalvaro@gmail.com>
2021-03-04 12:17:05 -0500

    Loasers

--- spa/app/App.js
+++ spa/app/App.js
@@ -1,44 +1,16 @@
 import api from './helpers/wp_api.js'
 import {ajax} from './helpers/ajax.js'
+import {Title} from './component/Title.js'
+import {Loader} from './component/Loader.js'
 
 export function App(){
-    document.getElementById("root").innerHTML=`<h1> hola desde emerson App</h1>`
-    
-    ajax({
-        url:api.POST,
-        cbSuccess : (posts)=>{
-            console.log(posts)
-        }
-    });
 
-    ajax({
-        url:api.CATEGORIES,
-        cbSuccess : (categories)=>{
-            console.log(categories)   
-        }
-    });
-    
-    ajax({
-        url:api.POSTS,
-        cbSuccess : (posts)=>{
-            console.log(posts)   
-        }
-    });
+    const d = document,
+       $root = d.getElementById("root");
+       $root.appendChild(Title());
+       $root.appendChild(Loader())
 
-    ajax({
-        url:api.SEARCH,
-        cbSuccess : (search)=>{
-            console.log(search)   
-        }
-    });
-
-    ajax({
-        url:api.SITE,
-        cbSuccess : (site)=>{
-            console.log(site)   
-        }
-    });
-
-    console.log(api)
+  
+  
     
 }
