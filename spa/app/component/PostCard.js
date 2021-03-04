172ffe35c3dd823955db42cb8027d7fadec1894a
emersonpalacio <emersonpalaciootalvaro@gmail.com>
2021-03-04 14:47:18 -0500

    postss

--- spa/app/component/PostCard.js
+++ spa/app/component/PostCard.js
@@ -1,12 +1,18 @@
-export function PostCard(){
-    return `
+export function PostCard(props) {
+  let { date, title, slug, _embedded } = props;
+  let dateFormat = new Date(date).toLocaleString();
+//   let urlPoster = _embedded["wp:featuredmedia"]
+//     ? _embedded["wp:featuredmedia"][0].source_url
+//     : "../assets/logoTerminado512x512.png";
+
+  return `
      <article class="post-card">
-      <img  src="https://placeimg.com/200/200/any" alt ="">
-       <h2>Titulo del Post </h2>
+      <img  src="https://placeimg.com/200/200/any" alt ="${title.rendered}">
+       <h4>${title.rendered} </h4>
        <p>
-         <time datetime="">Fecha</time>
-         <a href="#">Ver publicacion</a>
+         <time datetime="${date}"> ${dateFormat}</time>
+         <a href="#/${slug}">Ver publicacion</a>
        </p>       
      </article>
-    ` ;
+    `;
 }
